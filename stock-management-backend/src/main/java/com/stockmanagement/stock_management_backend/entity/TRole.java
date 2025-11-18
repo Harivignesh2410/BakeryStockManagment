f597358ecd151e623ac2098c3fe7ac6f0fef7bee
Harivignesh2410 <gopiharivignesh@gmail.com>
2025-11-18 09:49:06 +0530

    role table alte

--- stock-management-backend/src/main/java/com/stockmanagement/stock_management_backend/entity/TRole.java
+++ stock-management-backend/src/main/java/com/stockmanagement/stock_management_backend/entity/TRole.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "tRole")
+@Table(name = "tRole", schema = "dbo")
 public class TRole {
 
     @Id
